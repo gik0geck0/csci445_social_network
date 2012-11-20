c07576a4c7337d79f7d554ac5830f405c1c85aad
Matt Buland <mbuland@mymail.mines.edu>
2012-11-19 22:30:15 -0700

    Added the login_page as the index. Decided to make it the index because it should be the entry point

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+	<head>
+	</head>
+	<body>
+		<div>
+			<h1>Welcome to YASMS!</h1>
+			<p>
+				YASMS is Yet Another Social Media Site! By having an account, you will be able to find friends who are online, chat with them, share photos with them, and view their status updates.
+			</p>
+			<p>
+				Why choose YASMS over other sites? Simple: unlike our competitors, we haven't been taken over by trolls, internet memes, and your parents.
+			</p>
+		</div>
+		<form name="loginForm" action="homePage.php" method="post">
+			<table border="1">
+				<tr>
+					<td>
+						Username: 
+					</td>
+					<td>
+						<input type="text" name="username">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Password:
+					</td>
+					<td>
+						<input type="password" name="password">
+					</td>
+				</tr>
+			</table>
+			<div>
+				<input id="submitButton" type="submit" value="Log Me In!"/>
+				<a href="createAccount.php">Need to create an account?</a>
+			</div>
+		</form>
+	</body>
+</html>
