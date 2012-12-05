7d00472bd6d5bab070e448cbbf19bd4c5d789db8
Matthew D. Buland <mbuland@gc297-09.mines.edu>
2012-12-05 02:07:13 -0700

    needs testing

--- profile.php
+++ profile.php
@@ -1,107 +1,111 @@
 <!DOCTYPE HTML>
 <html>
-<head>
-</head>
-<body>
-<?php include("checkLogin.php"); ?>
-<?php include("menu.php"); ?>
-<div id="statuses">
-</div>
-<div id="info">
-<?php
-/*TODO: make it so if the logged in user is the target user, they can edit their information
-add an add friend button if the logged in user is not the target user
-display the last 5 status updates of the target user
-*/
+	<head>
+	</head>
+	<body>
+		<?php include("checkLogin.php"); ?>
+		<?php include("menu.php"); ?>
+		<div id="statuses">
+		</div>
+		<div id="info">
+			<?php
+			/*TODO: make it so if the logged in user is the target user, they can edit their information
+			  add an add friend button if the logged in user is not the target user
+			  display the last 5 status updates of the target user
+			 */
 
-$user = $_SESSION['user'];
-$db = new mysqli('localhost', 'team10', 'tangerine', 'team10_social');
-if (mysqli_connect_errno()) {
-	echo 'Error: could not connect to database. Better luck next time, punk!';
-	exit;
-}
+			$user = $_SESSION['user'];
+			$db = new mysqli('localhost', 'team10', 'tangerine', 'team10_social');
+			if (mysqli_connect_errno()) {
+				echo 'Error: could not connect to database. Better luck next time, punk!';
+				exit;
+			}
 
-if(isset($_POST['target'])) {
-	$target_user = $_POST['target'];
-} elseif(isset($_GET['target'])) {
-	$target_user = $_GET['target'];
-} else {
-	echo 'error: no user requested ';
-	exit;
-}
+			if(isset($_POST['target'])) {
+				$target_user = $_POST['target'];
+			} elseif(isset($_GET['target'])) {
+				$target_user = $_GET['target'];
+			} else {
+				echo 'error: no user requested ';
+				exit;
+			}
 
-$info_query = "select FirstName, LastName, Email, Gender, Age, Location from users where UID = ".$target_user;
-$info = $db->query($info_query);
-if($info->num_rows != 0) {
-	$user_info = $info->fetch_assoc();
-	echo "<p>First Name: ".$user_info['FirstName']."</p>";
-	echo "<p>Last Name: ".$user_info['LastName']."</p>";
-	echo "<p>Gender: ".$user_info['Gender']."</p>";
-	echo "<p>Email: ".$user_info['Email']."</p>";
-	echo "<p>Age: ".$user_info['Age']."</p>";
-	echo "<p>Location: ".$user_info['Location']."</p>";
-} else {
-	echo "invalid user requested";
-}
-/*
-<form action="processFriend.php" method="POST" >
-<tr>
-<td>
-<a href="profile.php?target=<?= $row['UID'] ?>" ><?= $row['FirstName']." ".$row['LastName']?></a>
-</td>
-<td>
-Email: <?= $row['Email'] ?>
-</td>
-<td>
-Gender: <?= $row['Gender'] ?>
-</td>
-<td>
-Age: <?= $row['Age'] ?>
-</td>
-<td> <?
-if (!$row['user0'] && !$row['user1']) {
-# no friendship at all.
-	?>
-	<input type="submit" value="Send friendship request" />
-	<?
-} elseif ($row['user0'] && $row['user1']) {
-# you already have a mutual friendship
-	?>
-	You are already friends!
-	<?
-} elseif ($row['user0']) {
-# you have already sent out a pending request
-	?>
-	Waiting for acceptance
-	<?
-} else {
-# they are awaiting a response
-	?>
-	Would you like to be his/her friend?
-	<table>
-	<tr>
-	<td>Accept</td>
-	<td>Decline</td>
-	</tr>
-	<tr>
-	<td><input type="radio" name="acceptDeny" value="2"></td>
-	<td><input type="radio" name="acceptDeny" value="0"></td>
-	<td><input type="submit" value="Send Response"></td>
-	</tr>
-	</table>
-	<?
-}
+			$info_query = "select FirstName, LastName, Email, Gender, Age, Location from users where UID = ".$target_user;
+			$info = $db->query($info_query);
+			if($info->num_rows != 0) {
+				$user_info = $info->fetch_assoc();
+				echo "<p>First Name: ".$user_info['FirstName']."</p>";
+				echo "<p>Last Name: ".$user_info['LastName']."</p>";
+				echo "<p>Gender: ".$user_info['Gender']."</p>";
+				echo "<p>Email: ".$user_info['Email']."</p>";
+				echo "<p>Age: ".$user_info['Age']."</p>";
+				echo "<p>Location: ".$user_info['Location']."</p>";
+			} else {
+				echo "invalid user requested";
+			}
 
-if ($usr && $tgt) {
-	?>
-	<input type="hidden" name="user" value="<?=$row['user0'] ?>" />
-	<input type="hidden" name="target" value="<?=$row['tgt0'] ?>" />
-	<? } ?>
-	</td>
-	</tr>
-	</form>
-	*/
-	?>
-	</div>
+			$friendshipQuery = "SELECT * FROM friendships WHERE (User = ".$user." AND Target = ".$target_user.") OR ( User = ".$target_user." AND Target = ".$user." ";
+			$freindshipResult = $db->query($friendshipQuery);
+			if (!$result) {
+				echo "Cannot search for your friendship status right now. Please try again later.<br/>";
+				# echo "Error: ".$db->error;
+				exit();
+			}
+			
+			$userOnTarget = -1;
+			$targetOnUser = -1;
+			while ($row = $result->fetch_assoc()) {
+				# for each row...
+				if ($row['User'] == $user) {
+					# set user on target
+					$userOnTarget = $row['Status'];
+				} elseif ($row['User'] == $target_user) {
+					$targetOnUser = $row['Status'];
+				}
+			}
+			?>
+	   <form action="processFriend.php" method="POST" >
+			   <tr>
+					<td> <?
+						if ($targetOnUser == 1) {
+							# they are awaiting a response
+							?>
+							Would you like to be his/her friend?
+							<table>
+								<tr>
+									<td>Accept</td>
+									<td>Decline</td>
+								</tr>
+								<tr>
+									<td><input type="radio" name="acceptDeny" value="2"></td>
+									<td><input type="radio" name="acceptDeny" value="0"></td>
+									<td><input type="submit" value="Send Response"></td>
+								</tr>
+							</table>
+							<?
+						} elseif ($userOnTarget == 2 and $targetOnUser == 2) {
+							# you already have a mutual friendship
+							?>
+							You are already friends!
+							<?
+						} elseif ($userOnTarget == 1) {
+							# you have already sent out a pending request
+							?>
+							Waiting for acceptance
+							<?
+						} elseif ($userOnTarget <= 0 and $targetOnUser <= 0) {
+							# no friendship at all.
+							?>
+							<input type="submit" value="Send friendship request" />
+							<?
+						}
+						# else does nothing.. makes a problem more visible
+						?>
+						<input type="hidden" name="user" value="<?=$user ?>" />
+						<input type="hidden" name="target" value="<?=$target_user ?>" />
+					</td>
+				</tr>
+			</form>
+		</div>
 	</body>
-	</html>
+</html>
