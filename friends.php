d3027ee33ad12ffeef051be0c2b36de8b0b6ed15
John Kelly <johkelly@mymail.mines.edu>
2012-12-04 22:05:32 -0700

    Friends list splits confirmed and pending friendships.

--- friends.php
+++ friends.php
@@ -23,15 +23,41 @@
 	       $prepQuery = $db->prepare("SELECT users.FirstName, users.LastName, users.ImageID, users.Gender "
 	                                ."FROM users JOIN friendships "
 	                                ."WHERE friendships.user=? "
-	                                ."AND users.UID=friendships.target");
+	                                ."AND users.UID=friendships.target"
+	                                ."AND friendships.Status=2");
            $prepQuery->bind_param("i", $_SESSION['user']);
            $prepQuery->bind_result($FirstName, $LastName, $ImageID, $Gender);
            $prepQuery->execute();
 	    ?>
+	    <h4>Friends</h4>
 		<div id="friends">
 		    <?php
 		        while($prepQuery->fetch()){?>
-		            <div id="div<?=$FirstName?>" class="friend">
+		            <div id="friend<?=$FirstName?>" class="friend">
+		                <p><?=$FirstName?> <?=$LastName?></p>
+		                <p>Avatar: <?=$ImageID?></p>
+		                <p><?=$Gender?></p>
+	                </div>
+		        <?php
+		        }
+	        $prepQuery->close();
+	        ?>
+		</div>
+		<?php
+		   $prepQuery = $db->prepare("SELECT users.FirstName, users.LastName, users.ImageID, users.Gender "
+	                                ."FROM users JOIN friendships "
+	                                ."WHERE friendships.user=? "
+	                                ."AND users.UID=friendships.user"
+	                                ."AND friendships.Status=1");
+           $prepQuery->bind_param("i", $_SESSION['user']);
+           $prepQuery->bind_result($FirstName, $LastName, $ImageID, $Gender);
+           $prepQuery->execute();
+	    ?>
+	    <h4>Pending Friendships</h4>
+		<div id="pending">
+		    <?php
+		        while($prepQuery->fetch()){?>
+		            <div id="pending<?=$FirstName?>" class="friend">
 		                <p><?=$FirstName?> <?=$LastName?></p>
 		                <p>Avatar: <?=$ImageID?></p>
 		                <p><?=$Gender?></p>
