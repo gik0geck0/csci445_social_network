fee4a8005ff1597f6b7f3230b6ff2508875cdfbf
John Kelly <johkelly@mymail.mines.edu>
2012-12-04 18:35:26 -0700

    Friends page will now list all friends of current user in session.

--- friends.php
+++ friends.php
@@ -1,11 +1,45 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
+	    <link href="friends.css" rel="stylesheet">
 	</head>
 	<body>
 		<?php include("checkLogin.php"); ?>
 		<?php include("menu.php"); ?>
+		<?php
+            $db = new mysqli('localhost', 
+                               'team10', 
+                               'tangerine', 
+                               'team10_social');
+	        $db->select_db('team10_social');
+                               
+            // TODO: Make this more robustly visible to the user
+	        if ( mysqli_connect_errno( ) )  {
+		        echo 'db connect error on connect';
+		        $Success = false;
+		        $Errors[] = "Error connecting to server (databse).";
+	        }
+	        
+	       $prepQuery = $db->prepare("SELECT users.FirstName, users.LastName, users.ImageID, users.Gender "
+	                                ."FROM users JOIN friendships "
+	                                ."WHERE friendships.user=? "
+	                                ."AND users.UID=friendships.target");
+           $prepQuery->bind_param("i", $_SESSION['user']);
+           $prepQuery->bind_result($FirstName, $LastName, $ImageID, $Gender);
+           $prepQuery->execute();
+	    ?>
 		<div id="friends">
+		    <?php
+		        while($prepQuery->fetch()){?>
+		            <div id="div<?=$FirstName?>" class="friend">
+		                <p><?=$FirstName?> <?=$LastName?></p>
+		                <p>Avatar: <?=$ImageID?></p>
+		                <p><?=$Gender?></p>
+	                </div>
+		        <?php
+		        }
+	        $prepQuery->close();
+	        ?>
 		</div>
 	</body>
 </html>
