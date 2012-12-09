8ed9eb205512df8a71c446760e3548885773c28b
Matthew D. Buland <mbuland@gc297-09.mines.edu>
2012-12-09 11:42:15 -0700

    split the html header components into header.php, and used it for the index

--- headerLoggedin.php
+++ headerLoggedin.php
@@ -1,8 +1,3 @@
-<!DOCTYPE HTML>
-<html>
-	<head>
-	    <link href="team10Style.css" rel="stylesheet">
-	</head>
-	<body>
-		<?php include("checkLogin.php"); ?>
-		<?php include("menu.php"); ?>
+<?php include("header.php") ?>
+<?php include("checkLogin.php"); ?>
+<?php include("menu.php"); ?>
