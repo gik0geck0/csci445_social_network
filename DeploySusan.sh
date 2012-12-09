3714ea5fbf234e875f736617c8d0b56ae094cadf
John <johkelly@mines.edu>
2012-12-09 16:27:02 -0700

    Revert change to deploy script

--- DeploySusan.sh
+++ DeploySusan.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-scp * $1@susan.mines.edu:/csci445f12/team10/social/$1
-ssh $1@susan.mines.edu chown -R :team10 /csci445f12/team10/social/$1
-ssh $1@susan.mines.edu chmod -R g+wrx /csci445f12/team10/social/$1
+scp * $1@susan.mines.edu:/csci445f12/team10/social
+ssh $1@susan.mines.edu chown -R :team10 /csci445f12/team10/social
+ssh $1@susan.mines.edu chmod -R g+wrx /csci445f12/team10/social
